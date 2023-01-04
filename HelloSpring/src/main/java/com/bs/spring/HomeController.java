404728414852d6c6c7bf6355de449a7d1c2f3a0a
GDJ56 <GDJ56@DESKTOP-KE137PC>
2023-01-04 10:24:57 +0900

    0104

--- HelloSpring/src/main/java/com/bs/spring/HomeController.java
+++ HelloSpring/src/main/java/com/bs/spring/HomeController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.bs.spring.model.vo.Animal;
+import com.bs.spring.model.vo.Food;
 import com.bs.spring.model.vo.Person;
 
 /**
@@ -24,9 +25,11 @@ public class HomeController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
-	//등록되어있는 springbean은 필드로 선언해서 사용
+	//등록되어있는 springbean은 필드로 선언해서 사용 : Autowired
+	// 서버가 생성될 때 만들어지게 되어있음
+	
 	@Autowired
-	@Qualifier(value="alonge")
+	@Qualifier(value="alonge")//객체가 여러개일 때 하나만 지정해서 사용하기
 	private Animal a;
 
 	@Autowired
@@ -34,8 +37,11 @@ public class HomeController {
 	private Animal b;
 	
 	@Autowired
-	private Person yeonji;
+	@Qualifier(value="getDongmin")
+	private Person p;
 	
+	@Autowired(required=false)// 없으면 냅둬 but 잘 안씀
+	private Food food;
 	
 	
 	
@@ -62,8 +68,9 @@ public class HomeController {
 //		System.out.println(a);
 //		System.out.println("dog : "+b);
 		
-		System.out.println(yeonji);
+		System.out.println(p);
 		
+		System.out.println(food);
 		//메인화면을 출력해주는 mapping 메소드
 		// /WEB-INF/views/return값.jsp 
 		// -> request.getRequestDispatcher("/WEB-INF/views/return값.jsp ").foward(req,res);
--- HelloSpring/src/main/java/com/bs/spring/model/vo/Food.java
+++ HelloSpring/src/main/java/com/bs/spring/model/vo/Food.java
@@ -1,15 +1,23 @@
 package com.bs.spring.model.vo;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 import lombok.Data;
 
 @Data
-//어노테이션을 이용해서 SpringBean으로 등록하기
+//어노테이션을 이용해서 SpringBean으로 등록하기 : Component
 @Component
 public class Food {
 	private String name;
 	private int price;
 	private String type;
+	private Person p;
 	
+	@Autowired
+	@Qualifier(value="yeonji")//Person이 여러명일 때 지정해서 사용하기
+	public void setPerson(Person p) {
+		this.p=p;
+	}
 }
